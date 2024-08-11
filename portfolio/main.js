50519ddf5198587c26b02d2746c87111840faba7
Gahlot <117899015+shoote@users.noreply.github.com>
2024-08-11 13:26:38 +0530

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.multiple-text',{
+    strings: ['Frontend Developer', 'Video Editor'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+    
+});
